73ac00aee7df4f4e8e85826af6ed6691f9557ef7
cy601@vip.qq.com <43111080+cy601@users.noreply.github.com>
2020-04-13 22:05:34 +0800

    finish city css

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+// 导入公用
+@import '~styles/varibles.styl';
+
+.list {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  right: 0;
+  top: 1.58rem;
+  bottom: 0;
+  width: 0.4rem;
+
+}
+
+.list .item {
+  text-align: center;
+  line-height: 0.4rem;
+  color: $bgColor;
+}
+</style>
